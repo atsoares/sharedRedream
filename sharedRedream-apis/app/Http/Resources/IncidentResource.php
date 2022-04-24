15b85ea91f45f65ca3b780b6e86571a05b835d20
Alexander <aleexsoarees@gmail.com>
2022-04-24 15:09:23 -0300

    fixing missing name

--- sharedRedream-apis/app/Http/Resources/IncidentResource.php
+++ sharedRedream-apis/app/Http/Resources/IncidentResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class IncidentsResource extends JsonResource
+class IncidentResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
