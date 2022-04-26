31eb6f70fccf6627f5993f8f1a1937a672e32fa4
Alexander <aleexsoarees@gmail.com>
2022-04-25 23:21:11 -0300

    refactoring repositories

--- sharedRedream-apis/app/Repositories/Impl/WalletRepositoryInterface.php
+++ sharedRedream-apis/app/Repositories/Impl/WalletRepositoryInterface.php
@@ -7,7 +7,7 @@ interface WalletRepositoryInterface
     public function findById(int $id);
     public function findByUserId(int $user_id);
     public function create(array $data);
-    public function deposit(int $user_id, int $value);
-    public function withdrawal(int $user_id, int $value);
+    public function deposit(int $user_id, float $value);
+    public function withdrawal(int $user_id, float $value);
 
 }
