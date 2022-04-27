4ae51030f1caaf1d1397361e8b62a947edbdfefb
Alexander <aleexsoarees@gmail.com>
2022-04-27 17:36:28 -0300

    refactoring AuthController with requests

---
+++ sharedRedream-apis/app/Http/Requests/SupportIncidentRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SupportIncidentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'user_id' => 'required',
+            'value' => 'required|min:1|max:100'
+        ];
+
+        return $rules;
+    }
+}
