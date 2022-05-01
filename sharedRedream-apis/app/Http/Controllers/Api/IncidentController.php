4b87a736458561d3ae86538ed885f3de66c77be3
Alexander <aleexsoarees@gmail.com>
2022-05-01 01:09:47 -0300

    remove unused import

--- sharedRedream-apis/app/Http/Controllers/Api/IncidentController.php
+++ sharedRedream-apis/app/Http/Controllers/Api/IncidentController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\Incident;
 use App\Services\IncidentService;
 use App\Http\Requests\StoreUpdateIncidentRequest;
