65e4ab7056642b7d163eaae25b908bf4ba4d7ecd
Alexander <aleexsoarees@gmail.com>
2022-05-04 21:16:01 -0300

    removing unused imports

--- sharedRedream-apis/routes/api.php
+++ sharedRedream-apis/routes/api.php
@@ -1,12 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\{
     AuthController,
     IncidentController,
     RedeemVoucherController,
-    WalletController,
     TransactionController
 };
 
